6e7b8846c03fff5f50a0e4c3a11a5ad813fc2cb3
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-08-25 23:51:32 -0400

    added full url as test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,51 @@
 import React from 'react'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import Leaderboard from './components/generalComponents/Leaderboard';
 
 
-function App() { 
-  const [backendData,setBackendData] = React.useState();
+function App() {
+  const [backendData, setBackendData] = React.useState();
   const [loading, setLoading] = React.useState(true)
   const navigate = useNavigate()
-  const [leaderboardTab,setLeaderboardTab] = React.useState()
- 
+  const [leaderboardTab, setLeaderboardTab] = React.useState()
 
-  React.useEffect(()=>{
-    fetch("/api")
-    .then(res => res.json())
-    .then(data => setBackendData(data))
-    .catch(error => console.error(error))
-    .finally(() => setLoading(false))
-  },[])
+
+  React.useEffect(() => {
+    fetch("https://mjis-wheres-waldo.fly.dev/api")
+      .then(res => res.json())
+      .then(data => setBackendData(data))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false))
+  }, [])
 
   // React.useEffect(()=>{ backendData && console.log(backendData)},[backendData])
 
-  function handleClick(id){
+  function handleClick(id) {
     navigate(`/gameboard/${id}`)
   }
 
   const gameBoardMapped = backendData && backendData.gameBoard.map(game => {
-    return(
+    return (
       <div key={game._id} className='homepage-game-holder'>
         <img className="homepage-image"
-        src={game.image}
-        alt="Game Image"
-        onClick={()=>handleClick(game._id)}
+          src={game.image}
+          alt="Game Image"
+          onClick={() => handleClick(game._id)}
         />
         <h3 className='homepage-game-name p sans-dm'>{game.name}</h3>
-       </div>
-      )
+      </div>
+    )
   })
 
   //*-------------------------------------------------------LeaderBoard tabs 
-  function handleTabChange(id){
+  function handleTabChange(id) {
     setLeaderboardTab(id)
   }
 
   const leaderboardSelectorMapped = backendData && backendData.gameBoard.map((map) => {
     return (
       <button
-        onClick={()=>handleTabChange(map._id)}
+        onClick={() => handleTabChange(map._id)}
         key={map._id}
         id={map._id}
         className='tablist-item fjord'
@@ -53,7 +53,7 @@ function App() {
     )
   })
 
-  if(loading){
+  if (loading) {
     return <p>Loading...</p>
   }
 
@@ -70,7 +70,7 @@ function App() {
           <div className='tablist-map'>
             {leaderboardSelectorMapped}
           </div>
-          <Leaderboard id={leaderboardTab}/>
+          <Leaderboard id={leaderboardTab} />
         </div>
       </div>
     </div>
